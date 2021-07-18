b162c15fd7b4f5c3234c8809f3068b61a8b36bd1
Anatoliy Tsvetkov <antsvetkov@gmail.com>
2021-07-18 17:39:47 +0300

    delete comment

--- bmi_calculator_flutter/lib/reusable_card.dart
+++ bmi_calculator_flutter/lib/reusable_card.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class ReusableCard extends StatelessWidget {
   ReusableCard({required this.cardColor, this.cardChild, this.onPress});
   final Color cardColor;
-  //тип Dynamic иначе требует  @required что мне нафиг не нужно
   final cardChild;
   final onPress;
 
